134514c3e0efb04ab90645975e8c5a21b308a2b5
YGseol <tjfdbsrl4563@gmail.com>
2024-10-13 23:11:44 +0900

    자바 스프링부트 백엔드서버 postgresql 구현

---
+++ src/test/java/ygseol/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package ygseol.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+     void contextLoads() {
+    }
+
+}
